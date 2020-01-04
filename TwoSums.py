57ab98d019724aedde14b8bf185b35983f74ed0a
patkhai <patrick.phyo@gmail.com>
2020-01-03 19:49:20 -0800

    twoSums

---
+++ TwoSums.py
@@ -0,0 +1,23 @@
+#O(n^2)
+def BruteForceTWOSUM(nums, target): 
+    for i in range(len(nums)): 
+        for j in range(i+1, len(nums)): 
+            sums = nums[i] + nums[j]
+            if sums == target: 
+                return [i,j]
+
+#O(n)
+def TwoSum(nums, target):
+    dic = dict() 
+    for i in range(len(nums)): 
+        complement = target - nums[i] 
+        if complement in dic: 
+            return [dic[complement], i]
+        dic[nums[i]] = i  #keep adding the cur num and index in DIC 
+
+num = [2,7,11,15]
+target = 18 
+
+#return [7,11]
+print(BruteForceTWOSUM(num,target))
+print(TwoSum(num,target))

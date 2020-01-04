daf97a8f77db81e299db77755be5fe217df916f2
patkhai <patrick.phyo@gmail.com>
2020-01-04 00:33:52 -0800

    added comments

--- TwoSums.py
+++ TwoSums.py
@@ -15,9 +15,30 @@ def TwoSum(nums, target):
             return [dic[complement], i]
         dic[nums[i]] = i  #keep adding the cur num and index in DIC 
 
+#O(n) with enumerate 
+def TwoSum_enumerate(nums, target):
+    dic = dict()
+    for i, n in enumerate(nums): 
+        complement = target - n 
+        if complement in dic: 
+            return [dic[complement], i]
+        dic[n] = i  #keep adding the cur num and index in DIC 
+
 num = [2,7,11,15]
 target = 18 
 
 #return [0,1] #indexs = 2, 7 
 print(BruteForceTWOSUM(num,target))
+print(TwoSum_enumerate(num,target))
 print(TwoSum(num,target))
+
+
+''' 
+create a dict 
+loop through the range and len of the num list 
+get the value of the target minus first index of the num list 
+if the value you get from subtracting is in the dict that you created 
+    return the index from the dic and the index of the list you are in 
+else just set the dic index of the num list into your index of the list you are currently in 
+
+''' 
